4bdf46a32057b56cab191587f1a33b727a8453c3
kelvinskevo <kelvinchemiati@gmail.com>
2023-02-17 05:41:08 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,13 +17,16 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n",
 			n, n % 10);
+	}
 	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
 		printf("LAst digit of %d is %d and is less than 6 and not 8\n"
 			n, n % 10);
+	}
 	else
 	{
 		printf("Last digit of %d is %d and is 0\n",
 			n, n % 10);
+	}
 	return (0);
 }
