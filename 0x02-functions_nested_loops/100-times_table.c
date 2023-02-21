e56d69800c804ca5d991a473aebecfacf87b3780
kelvinskevo <kelvinchemiati@gmail.com>
2023-02-21 12:51:08 -0800

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the times table of a given size
+ * @n: size of times table (0 <= n <= 15)
+ */
+void print_times_table(int n)
+{
+	int i, j, res;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				res = i * j;
+
+				if (j == 0)
+					_putchar('0');
+				else if (res < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(res + '0');
+				}
+				else if (res < 100)
+				{
+					_putchar(' ');
+					_putchar(res / 10 + '0');
+					_putchar(res % 10 + '0');
+				}
+				else
+				{
+					_putchar(res / 100 + '0');
+					_putchar((res / 10) % 10 + '0');
+					_putchar(res % 10 + '0');
+				}
+
+				if (j < n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
+
