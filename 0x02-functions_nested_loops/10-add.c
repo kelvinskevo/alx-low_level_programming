45957b0307da35cbb7b1acfc0a61033f08759427
kelvinskevo <kelvinchemiati@gmail.com>
2023-02-21 12:17:19 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @a:value to be added b
+ * @b:value to be added to a
+ *
+ * Return: sum of a and be
+ */
+
+int add(int a, int b)
+{
+	int d;
+
+	d = a + b;
+	return (d);
+}
