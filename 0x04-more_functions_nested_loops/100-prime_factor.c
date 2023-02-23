9a36508ad650d1733cfd35900f5096d242671a4e
kelvinskevo <kelvinchemiati@gmail.com>
2023-02-23 04:44:57 -0800

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,13 +2,13 @@
 #include "main.h"
 
 /**
- * main - prints the largest prime factor of 973764251143
+ * main - prints the largest prime factor of 612852475143
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	unsigned long int i, n = 973764251143;
+	unsigned long int i, n = 612852475143;
 
 	for (i = 3; i < 98651; i = i + 2)
 	{
