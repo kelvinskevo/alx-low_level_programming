fd324c01ce79f2215a23060b893b437735ce8ad1
kelvinskevo <kelvinchemiati@gmail.com>
2023-02-23 03:42:49 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+i#include "main.h"
+
+/**
+ * print_line - draws straight line
+ * @n: number of times _ printed
+ *
+ * Return: void
+ */
+
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
