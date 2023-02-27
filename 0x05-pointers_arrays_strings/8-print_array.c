78d23d82f36e45ebc9e90c995e2fac9908161f19
kelvinskevo <kelvinchemiati@gmail.com>
2023-02-27 10:44:44 -0800

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array -prints n elements of an array
+ * @a: array name
+ * @n: number of times of array to br printed
+ *
+ * Return: a nd n input
+ */
+
+void print_array(int *a, int *n)
+{
+	int j;
+
+	for (j = 0; j > (n - 1); i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	if (j == (n - 1))
+	{
+		printf("%d", a[n - 1]);
+	}
+	prinf("\n");
+}
