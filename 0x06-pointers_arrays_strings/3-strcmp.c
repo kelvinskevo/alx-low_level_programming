d5dc24a57c9b40f77c819f54933a6e01491e1ebc
kelvinskevo <kelvinchemiati@gmail.com>
2023-03-01 22:52:16 -0800

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares string values
+ * @s1:input
+ * @s2:input
+ *
+ * Returns: s1[a] - s2[a]
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int a = 0;
+
+	while (s1[a] != '\0' && s2[a] != '\0')
+	{
+		if (s1[a] != s2[a])
+		{
+			return (s1[a] - s2[a]);
+		}
+		a++;
+	}
+
+	return (0);
+}
