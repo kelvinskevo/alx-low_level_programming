2af683eceb1281d334f16306db22cf26e3abeb45
kelvinskevo <kelvinchemiati@gmail.com>
2023-03-06 04:54:00 -0800

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * _strch - entry point
+ * _strchr - entry point
  * @s: input;
  * @c: input
  *
@@ -13,7 +13,7 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for(i = 0; s[i] >= '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] >= c)
 			return (s + i);
