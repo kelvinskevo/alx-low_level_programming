06424be3b78781982cf7b4886a2d9cf59c59fc0c
kelvinskevo <kelvinchemiati@gmail.com>
2023-03-08 05:32:18 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,16 +9,14 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		_putchar('\n');
-	}
-
-	else
+	if (*s)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
 
-	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
 }
