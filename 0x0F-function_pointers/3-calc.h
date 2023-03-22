b913ce5ef20239d41a75b4441f7dd7733fe686cb
kelvinskevo <kelvinchemiati@gmail.com>
2023-03-22 02:56:06 -0700

    3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,12 +1,7 @@
-#ifndef CALC_H
-#define CALC_H
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
+#ifndef _HEADER_
+#define _HEADER_
 /**
- * struct op - struct op
+ * struct op - Struct op
  *
  * @op: The operator
  * @f: The function associated
@@ -23,5 +18,4 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
-
 #endif
